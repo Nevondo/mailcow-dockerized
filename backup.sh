79e34a7d1e72cccfc5441c1e71eaf29de4a143e5
Kevin Stenzel <mail@kevin-stenzel.de>
2018-01-06 19:03:56 +0100

    add archiv to backup

--- backup.sh
+++ backup.sh
@@ -2,24 +2,22 @@
 
 source backup/backup.conf
 
+#Einbinden der localen Backupkonfiguration die nicht überschrieben wird.
 if [[ -f "backup/backup.local.conf" ]]; then
 	source backup/backup.local.conf
 fi
-
-
-echo $BACKUPDIR
 
 DATE=$(date +"%Y%m%d_%H%M%S")
 
 function check_structure {
 
-	if [[ -f $BACKUPDIR ]]; then
+	if [[ -d $BACKUPDIR ]]; then
 		echo "Das angegebene Backup Verzeichnis: " $BACKUPDIR
 	else 
 		mkdir $BACKUPDIR
 	fi
 
-	if [[ -f $ARCHIVDIR ]]; then
+	if [[ -d $ARCHIVDIR ]]; then
 		echo "Das angegebene Archiv Verzeichnis: " $ARCHIVDIR
 	else 
 		mkdir $ARCHIVDIR
@@ -29,25 +27,40 @@ function check_structure {
 }
 
 function archiv_move {
-	mv $BACKUPDIR/backup_* $ARCHIVDIR/
+	mv $BACKUPDIR/backup_* $ARCHIVDIR/ >
 }
 
-function backup_mysql {
+function del_live_backup {
+	rm $BACKUPDIR/backup_*
+}
 
+function del_old_backups {
+
+	find $ARCHIVDIR -mtime +ARCHIV_HISTORY -exec rm {} \;
+
+}
+
+# MySQL Dump erstellen
+function backup_mysql {
 	docker-compose exec -T mysql-mailcow mysqldump --default-character-set=utf8mb4 -u${DBUSER} -p${DBPASS} ${DBNAME} > backup_${DBNAME}_${DATE}.sql
     mv backup_${DBNAME}_${DATE}.sql $BACKUPDIR/backup_${DBNAME}_${DATE}.sql
 	echo "MySQL Backup erfolgreich abgeschlossen."	
 }
 
+# Mailboxen Packen 
 function backup_maildir {
-
 	docker run --rm -i -v $(docker inspect --format '{{ range .Mounts }}{{ if eq .Destination "/var/vmail" }}{{ .Name }}{{ end }}{{ end }}' $(docker-compose ps -q dovecot-mailcow)):/vmail -v ${PWD}:/backup debian:stretch-slim tar cvfz /backup/backup_vmail.tar.gz /vmail
 	mv backup_vmail.tar.gz $BACKUPDIR/backup_vmail_${DATE}.tar.gz
-	echo "MySQL Backup erfolgreich abgeschlossen."	
+	echo "Mailboxen erfolgreich gesichert."	
 }
 
 check_structure
-archiv_move
+if [[ $ENABLE_ARCHIV == "yes" ]]; then 
+	archiv_move
+    del_old_backups
+else 
+	del_live_backup
+fi
 backup_mysql
 backup_maildir
 

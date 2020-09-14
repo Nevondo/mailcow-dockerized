69f9229260e2df89563cddb05f0161974416c60b
Aiko Appeldorn <aiko@appeldorn.dev>
2020-09-14 18:01:41 +0200

    o reverted math

--- data/web/js/build/014-mailcow.js
+++ data/web/js/build/014-mailcow.js
@@ -16,10 +16,10 @@ $(document).ready(function() {
     event.preventDefault();
     $('[data-hibp]').trigger('input');
     if (typeof($(this).closest("form").data('pwgen-length')) == "number") {
-      var random_passwd = Math.random().toString(36).slice($(this).closest("form").data('pwgen-length'))
+      var random_passwd = GPW.pronounceable($(this).closest("form").data('pwgen-length'))
     }
     else {
-      var random_passwd = Math.random().toString(36).slice(-13)
+      var random_passwd = GPW.pronounceable(12)
     }
     $(this).closest("form").find('[data-pwgen-field]').attr('type', 'text');
     $(this).closest("form").find('[data-pwgen-field]').val(random_passwd);

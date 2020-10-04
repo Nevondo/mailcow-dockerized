db532bf00eb4390eac0a578e176e6450b93cf352
Aiko Appeldorn <aiko@appeldorn.dev>
2020-10-04 18:36:42 +0200

    o removed expunge_trash script

--- helper-scripts/expunge_trash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Path to mailcow-dockerized, e.g. /opt/mailcow-dockerized
-cd ..
-
-/usr/local/bin/docker-compose exec -T dovecot-mailcow doveadm expunge -A mailbox 'Trash' savedbefore 180d

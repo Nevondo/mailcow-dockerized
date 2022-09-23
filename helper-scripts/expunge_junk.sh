9a5ce849c15c8fbc8a1089b40df9cac9772f0f5d
Aiko Appeldorn <aiko@appeldorn.dev>
2022-09-23 12:41:06 +0200

    o changed expung script

--- helper-scripts/expunge_junk.sh
+++ helper-scripts/expunge_junk.sh
@@ -2,4 +2,4 @@
 # Path to mailcow-dockerized, e.g. /opt/mailcow-dockerized
 cd ..
 
-/usr/local/bin/docker-compose exec -T dovecot-mailcow doveadm expunge -A mailbox 'Junk' savedbefore 4w
+/usr/local/bin/docker-compose exec -T dovecot-mailcow doveadm expunge -A mailbox 'Junk' savedbefore 30d
